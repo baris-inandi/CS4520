de367e3226d6e84207e91fb685c3acbf697d6c90
Baris Inandioglu <68742481+baris-inandi@users.noreply.github.com>
2026-05-15 11:22:58 +0200

    unrelated stuff

--- A3_Inandioglu_6696/app/src/test/java/com/itsbaris/cs4520/a3/ExampleUnitTest.kt
+++ A3_Inandioglu_6696/app/src/test/java/com/itsbaris/cs4520/a3/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package com.itsbaris.cs4520.a3
 
-import org.junit.Test
-
 import org.junit.Assert.*
+import org.junit.Test
 
 /**
  * Example local unit test, which will execute on the development machine (host).
